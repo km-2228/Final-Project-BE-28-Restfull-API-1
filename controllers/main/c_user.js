32a887012d246a42e2a077028e6324c7e7e5b2bf
Nurul Hikmah Rafika Dewi <nurulhikmah.160501@gmail.com>
2022-11-23 10:30:49 +0700

    Nurul-Comment,Thread,User

--- controllers/main/c_user.js
+++ controllers/main/c_user.js
@@ -6,7 +6,17 @@ const editProfile = async (req, res) => {
 }
 
 const getProfile = async (req, res) => {
+    try {
+        const _idUser = req.user.user._id
+        await User.findOne({"_id":_idUser}, (err, result) => {
+            if(err) return res_error(res, 400, "400 Bad Request", "Request error by client so that it cannot get profile")
 
+            return res_success(res, 200, "200 OK", "Your data was checked", result)
+        }).populate('role country', "role country").clone().catch(err => console.log(err))
+        
+    } catch (error) {
+        if(error) return res_error(res, 500, "500 Internal Server Error",error.message)
+    }
 }
 
 const updateImage = async (req, res) => {
