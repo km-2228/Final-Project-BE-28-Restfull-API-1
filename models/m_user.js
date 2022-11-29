b35afa5631099a9791ed82f8573f5c834875dfcc
hafiihzafarhana <hafiihza2002@gmail.com>
2022-11-29 18:46:20 +0700

    Hafi - img user

--- models/m_user.js
+++ models/m_user.js
@@ -31,7 +31,7 @@ const UserSchema = mongoose.Schema({
      },
      image:{
       type:String,
-      default:"https://www.clipartkey.com/mpngs/m/152-1520367_user-profile-default-image-png-clipart-png-download.png"
+      default:process.env.IMAGEPROFILE
      }
 }, { timestamps: true });
 
