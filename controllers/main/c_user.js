de2364c82b8ab5aa436e31d22b399eccfc8e5ac3
hafiihzafarhana <hafiihza2002@gmail.com>
2022-11-30 14:58:17 +0700

    Hafi - password lagi

--- controllers/main/c_user.js
+++ controllers/main/c_user.js
@@ -1,13 +1,14 @@
 const Thread = require('../../models/m_thread');
 const User = require('../../models/m_user');
+const bcrypt = require('bcryptjs');
 const {res_error, res_success} = require('../../response')
 
 const editProfile = async (req, res) => {
      try {
-        const data = req.body;
+        const {country} = req.body;
         const _idUser = req.user.user._id
         await User.updateOne({"_id":_idUser}, 
-        {$set:data}, (err, result) => {
+        {$set:{"country":country}}, (err, result) => {
             if(err) return res_error(res, 400, "400 Bad Request", "Request error by client so that it cannot change profile")
 
             return res_success(res, 200, "200 OK", "Your profile was changed")
@@ -15,6 +16,22 @@ const editProfile = async (req, res) => {
     } catch (error) {
         if(error) return res_error(res, 500, "500 Internal Server Error",error.message)
     }   
+}
+
+const editPassword = async (req, res) => {
+    try {
+        const {password:textPass} = req.body;
+        const password = await bcrypt.hash(textPass, 10);
+        const _idUser = req.user.user._id
+        await User.updateOne({"_id":_idUser}, 
+        {$set:{"password":password}}, (err, result) => {
+            if(err) return res_error(res, 400, "400 Bad Request", "Request error by client so that it cannot change profile")
+
+            return res_success(res, 200, "200 OK", "Your profile was changed")
+        }).clone().catch(err => console.log(err))
+    } catch (error) {
+        if(error) return res_error(res, 500, "500 Internal Server Error",error.message)
+    }
 }
 
 const getProfile = async (req, res) => {
@@ -61,4 +78,4 @@ const threadUser = async (req, res) => {
     }
 }
 
-module.exports = {editProfile, getProfile, updateImage, threadUser}
+module.exports = {editProfile, getProfile, updateImage, threadUser, editPassword}
